b3d604284cce0469733165f15b7f46b4b4ce0a64
baiyanli <baiyanli@weidian.com>
2016-10-16 11:32:47 +0800

    Commit changes

--- 1-Fundamentials/1-1-ProgrammingModel/Exer3.java
+++ 1-Fundamentials/1-1-ProgrammingModel/Exer3.java
@@ -1,10 +1,14 @@
-public class Exer3
-{
-	public static void main(String[] arge)
-	{
-		System.out.println((1+2.236)/2);
-		System.out.println(1+2+3+4.0);
-		System.out.println(4.1>=4);
-		System.out.println(1+2+"3");
-	}
+import java.util.Scanner;
+public class Exer3 {
+ public static void main(String[] arge) {
+     Scanner scanner = new Scanner(System.in);
+     int num1 = scanner.nextInt();
+     int num2 = scanner.nextInt();
+     int num3 = scanner.nextInt();
+     if( (num1 == num2) && (num2 == num3) ){
+         System.out.println("equal");
+     }else{
+         System.out.println("not equal");
+     }
+ }
 }
